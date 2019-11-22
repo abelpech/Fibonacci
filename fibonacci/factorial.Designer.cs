fa102527dd7a7eb32eaf83c94b9d87e5af12f22d
Abel Pech <abel.pech@gmail.com>
2019-11-21 21:32:25 -0800

    corrected function for fibonacci and created for factorial number!

--- fibonacci/factorial.Designer.cs
+++ fibonacci/factorial.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace fibonacci
 {
-    partial class fibonacci
+    partial class factorial
     {
         /// <summary>
         /// Required designer variable.
@@ -35,19 +35,17 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnCalcular = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.btnLimpiar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(215, 35);
+            this.label1.Location = new System.Drawing.Point(290, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(335, 51);
+            this.label1.Size = new System.Drawing.Size(197, 51);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Serie Fibonacci";
+            this.label1.Text = "Factorial";
             // 
             // label2
             // 
@@ -61,14 +59,12 @@
             // 
             // listBox1
             // 
-            this.listBox1.ColumnWidth = 200;
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 29;
             this.listBox1.Location = new System.Drawing.Point(42, 415);
-            this.listBox1.MultiColumn = true;
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(702, 178);
+            this.listBox1.Size = new System.Drawing.Size(669, 178);
             this.listBox1.TabIndex = 2;
             // 
             // label3
@@ -94,12 +90,11 @@
             // btnCalcular
             // 
             this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalcular.Location = new System.Drawing.Point(178, 254);
+            this.btnCalcular.Location = new System.Drawing.Point(191, 251);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(423, 90);
+            this.btnCalcular.Size = new System.Drawing.Size(389, 90);
             this.btnCalcular.TabIndex = 5;
             this.btnCalcular.Text = "CALCULAR";
-            this.btnCalcular.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.btnCalcular.UseVisualStyleBackColor = true;
             this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
@@ -114,35 +109,11 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(178, 613);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(452, 58);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Da un vistazo a FACTORIAL!";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // btnLimpiar
-            // 
-            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpiar.Location = new System.Drawing.Point(636, 373);
-            this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(108, 36);
-            this.btnLimpiar.TabIndex = 8;
-            this.btnLimpiar.Text = "LIMPIAR";
-            this.btnLimpiar.UseVisualStyleBackColor = true;
-            this.btnLimpiar.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // fibonacci
+            // factorial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 700);
-            this.Controls.Add(this.btnLimpiar);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(800, 605);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnCalcular);
             this.Controls.Add(this.textBox1);
@@ -151,7 +122,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "fibonacci";
+            this.Name = "factorial";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main";
             this.ResumeLayout(false);
@@ -168,8 +139,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button btnLimpiar;
     }
 }
 
--- fibonacci/fibonacci.cs
+++ fibonacci/fibonacci.cs
@@ -18,31 +18,41 @@ namespace fibonacci
         }
 
         //Funcion Recursiva 
-        private int fibonacciRecursiva(int n)
+        private void fibonacciResult(int number)
         {
-            if (n <= 1)
+            int n1 = 0, n2 = 1, n3 = 0;
+            listBox1.Items.Add("Iteracion #" + n1 + ":  "+ n1);
+            listBox1.Items.Add("Iteracion #" + n2 + ":  "+ n2);
+            for (int i = 2; i < number; ++i)
             {
-                return 1;
-            }
-            else
-            {
-                return (n * fibonacciRecursiva(n - 1));
+                n3 = n1 + n2;
+                listBox1.Items.Add("Iteracion #" + i + ":  " + n3);
+                
+                n1 = n2;
+                n2 = n3;
+
             }
         }
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-           
+            int numero=0;
             // Set the ListBox to display items in multiple columns.
             listBox1.MultiColumn = true;
             // Set the selection mode to multiple and extended.
             listBox1.SelectionMode = SelectionMode.MultiExtended;
             // Shutdown the painting of the ListBox as items are added.
             listBox1.BeginUpdate();
-            // Loop through and add 50 items to the ListBox.
-            for (int x = 0; x <= Convert.ToInt32(textBox1.Text); x++)
+                // Loop through and add 50 items to the ListBox.
+
+                //Cantidad SET
+            numero = Convert.ToInt32(textBox1.Text);
+            fibonacciResult(numero);
+            /*for (int x = 0; x <= Convert.ToInt32(textBox1.Text); x++)
             {
                 listBox1.Items.Add( x + "! = " + fibonacciRecursiva(x)); 
-            }
+            }*/
+
+
             // Allow the ListBox to repaint and display the new items.
             listBox1.EndUpdate();
 
@@ -52,6 +62,19 @@ namespace fibonacci
         private void button1_Click_1(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            factorial facto = new factorial();
+            facto.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox1.Text = "";
         }
     }
 }
